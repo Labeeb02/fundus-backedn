e00ceb7aa09070105bb1c3298b525c418c9f38f5
Labeeb02 <ahsan.labeeb@gmail.com>
2022-10-01 03:04:19 +0530

    Initial Commit

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const router=require('express').Router();
+const User=require('../models/User');
+const CryptoJS=require('crypto-js');
+const jwt=require('jsonwebtoken');
+const passport=require('passport');
+
+router.get('/google', passport.authenticate('google', { scope: ['profile','email'] }))
+
+router.get('/google/callback', passport.authenticate('google', 
+{failureRedirect: '/failed', session: false}), (req, res) => {
+// Fetch JWT from req.user
+const jwt = req.user.token;
+const id=req.user.id;
+req.session = {jwt}
+// Successful authentication, redirect home
+//res.status(200).redirect('/home');
+res.send({id,jwt})
+});
+
+module.exports=router;
