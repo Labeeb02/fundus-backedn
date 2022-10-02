c6098219153afdf7bf5d0de249cb83021a1b860c
Labeeb02 <ahsan.labeeb@gmail.com>
2022-10-02 18:58:14 +0530

    addes lastAccessed 2.0

--- routes/auth.js
+++ routes/auth.js
@@ -11,13 +11,15 @@ router.get('/google/callback', passport.authenticate('google',
 // Fetch JWT from req.user
 const jwt = req.user.token;
 const id=req.user.id;
+const last=req.user.last;
 req.session = {jwt}
 // Successful authentication, redirect home
 //res.status(200).redirect('/home');
-res.send({id,jwt})
-// res.cookie(`jwt`,jwt);
-// res.cookie(`id`,id);
-// res.redirect('https://fantastic-madeleine-065d19.netlify.app');
+//res.send({id,jwt})
+res.cookie(`jwt`,jwt);
+res.cookie(`id`,id);
+res.cookie(`last`,last);
+res.redirect('https://fantastic-madeleine-065d19.netlify.app');
 
 });
 
